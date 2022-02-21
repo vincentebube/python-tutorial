c617dfa8143ce46808b401369fa28a190efea4d9
vincentebube <okutalukwee@gmail.com>
2022-02-21 14:48:24 +0100

    initial commit

---
+++ python works/my process.py
@@ -0,0 +1,16 @@
+print("Hello world")
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /__ |")
+character_name = "kate"
+character_age = "88"
+print("There was once a man named" + character_name + " , ")
+print("he was "+ character_age + "  years old. ")
+character_name = "zeus"
+print("He really liked the name" + character_name + " , ")
+print("but didn't  like being" + character_age + " , ")
+phrase = "My world "
+print(phrase.upper() .isupper())
+phrase = "Alive and good"
+print(len(phrase))
